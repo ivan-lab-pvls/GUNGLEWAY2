a9412990d49adbed57ab4f30ee17f6bba7bae986
ilobosok <121606196+ivan-lab-pvls@users.noreply.github.com>
2024-01-18 15:52:56 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flame/flame.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'core/app_export.dart';
+
+var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
+int initScreen = 0;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  initScreen = await prefs.getInt("initScreen") ?? 0;
+  await prefs.setInt("initScreen", 1);
+  Future.wait([
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]),
+    PrefUtils().init()
+  ]).then((value) {
+
+
+    runApp(MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return BlocProvider(
+          create: (context) => ThemeBloc(
+            ThemeState(
+              themeType: PrefUtils().getThemeData(),
+            ),
+          ),
+          child: BlocBuilder<ThemeBloc, ThemeState>(
+            builder: (context, state) {
+              return MaterialApp(
+                theme: theme,
+                title: 'gamble',
+                navigatorKey: NavigatorService.navigatorKey,
+                debugShowCheckedModeBanner: false,
+                localizationsDelegates: [
+                  AppLocalizationDelegate(),
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                ],
+                supportedLocales: [
+                  Locale(
+                    'en',
+                    '',
+                  ),
+                ],
+                initialRoute: initScreen == 0
+                    ? AppRoutes.onBoardingRoute
+                    : AppRoutes.initialRoute,
+                routes: AppRoutes.routes,
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
